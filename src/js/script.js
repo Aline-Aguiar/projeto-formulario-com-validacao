ca6289f0c246726eb23522a75444680cfe57fb0b
Aline-Aguiar <aline.aglilly@gmail.com>
2025-03-13 23:10:03 -0300

    criando btnEnviar no script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,7 @@
 const formulario = document.getElementById("formulario");
 const inputsRequeridos = document.querySelectorAll(".requerido"); /* array */
 const spans = document.querySelectorAll(".span-requerido"); /* array */
+const btnEnviar = document.getElementById("btn-enviar")
 
 let spanAtual = 0;
-
-
 
