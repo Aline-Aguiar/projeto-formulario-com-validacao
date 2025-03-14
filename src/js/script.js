a7f9e4ad6a77d9d351edea3ff40e5673ebf5bb7e
Aline-Aguiar <aline.aglilly@gmail.com>
2025-03-14 02:17:47 -0300

    ajustando e refatorando código de script.js

--- src/js/script.js
+++ src/js/script.js
@@ -3,32 +3,27 @@ const inputsRequeridos = document.querySelectorAll(".requerido"); /* array */
 const spans = document.querySelectorAll(".span-requerido"); /* array */
 const btnEnviar = document.getElementById("btn-enviar")
 
-// let inputAtual = 0;
-
-inputsRequeridos.forEach(function (input,indice){
+inputsRequeridos.forEach(function (input, indice) {
     input.addEventListener('input', () => {
 
         if (input.value === "") {
-            inputsRequeridos[indice].classList.add("campo-vazio");
-            inputsRequeridos[indice].classList.add("campo-vazio");
-            spans[indice].classList.remove("sem-erro");
-            spans[indice].classList.add("erro");
+            setarErro(indice)
         } else {
-            inputsRequeridos[indice].classList.remove("campo-vazio");
-            spans[indice].classList.remove("erro");
-            spans[indice].classList.add("sem-erro");
+            removerErro(indice)
         }
     })
 })
 
-// function setarErro() {
-//     inputsRequeridos[inputAtual].classList.add("campo-vazio");
-//     spans[inputAtual].classList.remove("sem-erro");
-//     spans[inputAtual].classList.add("erro");
-// }
+function setarErro(indice) {
+    inputsRequeridos[indice].classList.add("campo-vazio");
+    inputsRequeridos[indice].classList.remove("campo-preenchido");
+    spans[indice].classList.remove("sem-erro");
+    spans[indice].classList.add("erro");
+}
 
-// function removerErro() {
-//     inputsRequeridos[inputAtual].classList.remove("campo-vazio");
-//     spans[inputAtual].classList.remove("erro");
-//     spans[inputAtual].classList.add("sem-erro");
-// }
+function removerErro(indice) {
+    inputsRequeridos[indice].classList.remove("campo-vazio");
+    inputsRequeridos[indice].classList.add("campo-preenchido");
+    spans[indice].classList.remove("erro");
+    spans[indice].classList.add("sem-erro");
+}
