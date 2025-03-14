f570c185057c467c7ec45ad9203889f91fbb9079
Aline-Aguiar <aline.aglilly@gmail.com>
2025-03-14 01:05:09 -0300

    alterando script.js, usando indice

--- src/js/script.js
+++ src/js/script.js
@@ -3,5 +3,32 @@ const inputsRequeridos = document.querySelectorAll(".requerido"); /* array */
 const spans = document.querySelectorAll(".span-requerido"); /* array */
 const btnEnviar = document.getElementById("btn-enviar")
 
-let spanAtual = 0;
+// let inputAtual = 0;
 
+inputsRequeridos.forEach(function (input,indice){
+    input.addEventListener('input', () => {
+
+        if (input.value === "") {
+            inputsRequeridos[indice].classList.add("campo-vazio");
+            inputsRequeridos[indice].classList.add("campo-vazio");
+            spans[indice].classList.remove("sem-erro");
+            spans[indice].classList.add("erro");
+        } else {
+            inputsRequeridos[indice].classList.remove("campo-vazio");
+            spans[indice].classList.remove("erro");
+            spans[indice].classList.add("sem-erro");
+        }
+    })
+})
+
+// function setarErro() {
+//     inputsRequeridos[inputAtual].classList.add("campo-vazio");
+//     spans[inputAtual].classList.remove("sem-erro");
+//     spans[inputAtual].classList.add("erro");
+// }
+
+// function removerErro() {
+//     inputsRequeridos[inputAtual].classList.remove("campo-vazio");
+//     spans[inputAtual].classList.remove("erro");
+//     spans[inputAtual].classList.add("sem-erro");
+// }
