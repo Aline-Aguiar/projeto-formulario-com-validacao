3bd33eb56d6c00a428466069f317067718e0a2c9
Aline-Aguiar <aline.aglilly@gmail.com>
2025-03-25 21:43:28 -0300

    refatorando codigo do arquivo script.js, utilizando o metodo nextElementSibling

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,17 @@
-const inputsRequeridos = document.querySelectorAll(".requerido");
-const spans = document.querySelectorAll(".span-requerido");
-const btnEnviar = document.getElementById("btn-enviar")
+const camposFormulario = document.querySelectorAll(".requerido");
+const botaoEnviar = document.querySelector(".btn-enviar")
 
-btnEnviar.addEventListener("click", (e) => {
+botaoEnviar.addEventListener('click',(e)=>{
     e.preventDefault()
 
-    inputsRequeridos.forEach(function (input, indice) {
-        if (input.value.trim() !== "") {
-            removerErro(indice)
-        } else {
-            setarErro(indice)
+    camposFormulario.forEach((input)=>{
+        if(input.value){
+            input.classList.add('campo-preenchido')
+            input.nextElementSibling.classList.remove('erro')
+        }else{
+            input.classList.remove('campo-preenchido')
+            input.classList.add('campo-vazio')
+            input.nextElementSibling.classList.add('erro')
         }
     })
 })
-
-function setarErro(indice) {
-    inputsRequeridos[indice].classList.add("campo-vazio");
-    inputsRequeridos[indice].classList.remove("campo-preenchido");
-    spans[indice].classList.remove("sem-erro");
-    spans[indice].classList.add("erro");
-}
-
-function removerErro(indice) {
-    inputsRequeridos[indice].classList.remove("campo-vazio");
-    inputsRequeridos[indice].classList.add("campo-preenchido");
-    spans[indice].classList.remove("erro");
-    spans[indice].classList.add("sem-erro");
-}
